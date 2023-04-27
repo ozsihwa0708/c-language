2506f43fdd0e680c7e1f19994aad6ae103d3fded
이기주 <ozsihwa200@gmail.com>
2023-04-27 01:47:44 +0000

    2312

---
+++ 69.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+
+    int a=1;
+    printf("        1번만 출력하라\n");
+    printf("%9d번만 출력하라", a);printf("\n");
+    printf("\t%.0f번만 출력하라\n", 1.23);
+
+    return 0;
+}
